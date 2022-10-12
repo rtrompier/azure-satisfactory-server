7dc5c0d91f431c7ebfba06cbdbc8d32ff120d6db
rtrm <rtrm@hcuge.ch>
2022-10-12 14:56:50 +0200

    test

--- scripts/init.sh
+++ scripts/init.sh
@@ -69,6 +69,7 @@ if [[ -d "/datadisks/disk1" ]]; then
     echo "/datadisks/disk1 directory found"
 else
     echo "/datadisks/disk1 directory not found, create it"
+    mkdir /datadisks
     mkdir /datadisks/disk1
 fi
 
@@ -78,3 +79,17 @@ echo "Datadrive mounted"
 
 
 echo "Hello world" > /datadisks/disk1/hello.txt
+
+
+# if [[ -d "/datadisks/disk1/satisfactory" ]]; then
+#     echo "satisfactory directory found"
+# else
+#     echo "satisfactory directory not found, create it"
+#     mkdir /datadisks/disk1/satisfactory
+    
+#     mkdir /datadisks/disk1/satisfactory/config
+#     curl -o /datadisks/disk1/satisfactory/docker-compose.yml https://raw.githubusercontent.com/rtrompier/satisfactory-server-scaleway/main/satisfactory/docker-compose.yml
+    
+#     cd /datadisks/disk1/satisfactory
+#     docker-compose up -d
+# fi
