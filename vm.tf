d91eb618faffbed7d88701cdb8ef0dd212b1244e
rtrm <rtrm@hcuge.ch>
2022-10-12 11:19:58 +0200

    add disk

--- vm.tf
+++ vm.tf
@@ -32,18 +32,18 @@ resource "azurerm_linux_virtual_machine" "cicd" {
 
 }
 
-# resource "azurerm_managed_disk" "disk" {
-#   name                 = "lvm-we-d-cicd-datadisk"
-#   location             = azurerm_resource_group.ci.location
-#   resource_group_name  = azurerm_resource_group.ci.name
-#   storage_account_type = "Standard_LRS"
-#   create_option        = "Empty"
-#   disk_size_gb         = 10
-# }
+resource "azurerm_managed_disk" "disk" {
+  name                 = "lvm-we-d-cicd-datadisk"
+  location             = azurerm_resource_group.ci.location
+  resource_group_name  = azurerm_resource_group.ci.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 10
+}
 
-# resource "azurerm_virtual_machine_data_disk_attachment" "example" {
-#   managed_disk_id    = azurerm_managed_disk.disk.id
-#   virtual_machine_id = azurerm_linux_virtual_machine.cicd.id
-#   lun                = "10"
-#   caching            = "ReadWrite"
-# }
+resource "azurerm_virtual_machine_data_disk_attachment" "example" {
+  managed_disk_id    = azurerm_managed_disk.disk.id
+  virtual_machine_id = azurerm_linux_virtual_machine.cicd.id
+  lun                = "10"
+  caching            = "ReadWrite"
+}
