da34b2f5099236f174805c96b6dff87644ae3253
rtrm <rtrm@hcuge.ch>
2022-10-12 08:17:02 +0200

    test

--- main.tf
+++ main.tf
@@ -1,45 +1,25 @@
 terraform {
   required_providers {
-    scaleway = {
-      source = "scaleway/scaleway"
-      version = "2.4.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
     }
   }
 
   backend "remote" {
     organization = "rtrm"
     workspaces {
-      name = "satisfactory-server-scaleway"
+      name = "satisfactory-server"
     }
   }
 }
 
-provider "scaleway" {
-  # Configuration options
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
 }
 
-
-# resource "scaleway_instance_volume" "data" {
-#   size_in_gb = 100
-#   type = "b_ssd"
-# }
-
-resource "scaleway_instance_ip" "ip" {}
-
-# resource "scaleway_instance_server" "web" {
-#   type = "DEV1-S"
-#   image = "ubuntu_jammy"
-# 
-#   tags = [ "hello", "public" ]
-# 
-#   root_volume {
-#     delete_on_termination = false
-#   }
-# 
-#   additional_volume_ids = [ scaleway_instance_volume.data.id ]
-# 
-#   user_data = {
-#     foo        = "bar"
-#     cloud-init = file("./init.yml")
-#   }
-# }
+resource "azurerm_resource_group" "ci" {
+  name     = "RG-WE-D-CICD"
+  location = "West Europe"
+}
